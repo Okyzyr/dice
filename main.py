598a123b5ea2d1956729fa82c173e4e573032d47
tenteges <kcukierski@gmail.com>
2021-03-26 18:58:39 +0100

    works ok

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
+from random import randint
 import re
+
 
 def roll(rzut):
     dice_roll = re.split('[D+-]', rzut)
-    roll_multiplier = 0
-    modifier = 0
     if dice_roll[0] == "":
         dice_roll[0] = "1"
     roll_multiplier = int(dice_roll[0])
-    dice_type = "D"+dice_roll[1]
+    result = int(roll_multiplier) * randint(1, int(dice_roll[1]))
     if "-" in rzut:
         modifier = "-"+dice_roll[2]
+        result = int(roll_multiplier)*randint(1, int(dice_roll[1])-int(modifier))
     if "+" in rzut:
         modifier = "+"+dice_roll[2]
-
-
-    return f"Rodzaj kości: {dice_type}, ilośc rzutów: {roll_multiplier}, modyfikator: {modifier}",dice_roll
+        result = int(roll_multiplier) * randint(1, int(dice_roll[1]) + int(modifier))
+    return result
 
 
 print(roll("2D10+10"))
